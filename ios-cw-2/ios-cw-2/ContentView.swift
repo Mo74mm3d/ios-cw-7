aff32d838eba3f85c443466398c946a4416b7ae8
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-09 19:12:37 +0300

    Done + Bonus

--- ios-cw-2/ios-cw-2/ContentView.swift
+++ ios-cw-2/ios-cw-2/ContentView.swift
@@ -27,27 +27,40 @@ struct ContentView: View {
                 VStack {
                     Text("IOS")
                         .frame(width: 250, height: 60)
-                        .background(.black)// bonus change it to cyan
+                        .background(.cyan)
                         .clipShape(RoundedRectangle(cornerRadius: 35))
-                        // cahnge name to iOS
+                        .onTapGesture {
+                            name = "apple"
+                            
+                        }
+                        // change name to iOS
 
                     Text("Gamedev")
                         .frame(width: 250, height: 60)
-                        .background(.black)// bonus change it to red
+                        .background(.red)
                         .clipShape(RoundedRectangle(cornerRadius: 35))
-                        // cahnge name to Gamedev
+                        .onTapGesture {
+                            name = "gamedev"
+                        }
+                        // change name to Gamedev
 
                     Text("Web")
                         .frame(width: 250, height: 60)
-                        .background(.black)// bonus change it to blue
+                        .background(.blue)
                         .clipShape(RoundedRectangle(cornerRadius: 35))
-                        // cahnge name to web
+                        .onTapGesture {
+                            name = "web"
+                        }
+                        // change name to web
                         
                     Text("Android")
                         .frame(width: 250, height: 60)
-                        .background(.black)// bonus change it to green
+                        .background(.green)
                         .clipShape(RoundedRectangle(cornerRadius: 35))
-                        // cahnge name to Android
+                        .onTapGesture {
+                            name = "android"
+                        }
+                        // change name to Android
                 }
             
             }
